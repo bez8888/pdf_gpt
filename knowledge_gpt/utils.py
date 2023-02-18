437e6dd60bf78f3df493ec640c8c4875b9913ff9
bez8888 <116435167+bez8888@users.noreply.github.com>
2023-02-18 23:02:50 +0200

    Update utils.py

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -90,7 +90,7 @@ def embed_docs(docs: List[Document]) -> VectorStore:
 
     if not st.session_state.get("OPENAI_API_KEY"):
         raise AuthenticationError(
-            "Enter your OpenAI API key in the sidebar. You can get a key at https://platform.openai.com/account/api-keys."
+            "Введіть ваш ключ OpenAI API зліва в sidebar. Ви можете отримати ключ за лінком https://platform.openai.com/account/api-keys."
         )
     else:
         # Embed the chunks
